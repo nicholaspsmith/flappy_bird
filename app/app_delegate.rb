538395f4e285aaa96bc54a35cb47c6d646f486cd
Nicholas Smith <npsmith1990@gmail.com>
2016-03-26 22:38:03 -0500

    update app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,18 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'flappy'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
+    controller = GameViewController.alloc.init
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
+    @window.rootViewController = controller
     @window.makeKeyAndVisible
-
-    alert = UIAlertView.new
-    alert.message = "Hello world!"
-    alert.show
 
     true
   end
