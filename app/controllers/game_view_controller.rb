63e502d48aa698eecefb9263795a11aedba6907c
Nicholas Smith <npsmith1990@gmail.com>
2016-03-26 21:59:11 -0500

    create rubymotion app

---
+++ app/controllers/game_view_controller.rb
@@ -0,0 +1,24 @@
+class GameViewController < UIViewController
+  def loadView
+    view = SKView.new
+    view.showFPS = true
+    view.showsNodeCount = true
+    view.showsDrawCount = true
+
+    self.view = view
+  end
+
+  def prefersStatusBarHidden
+    true
+  end
+
+  def viewWillLayoutSubviews
+    super
+
+    unless self.view.scene
+      scene = SkyLinesScene.alloc.initWithSize(view.bounds.size)
+      scene.scaleMode = SKSceneScaleModeAspectFill
+      view.presentScene scene
+    end
+  end
+end
